63a4343a5fb3f88bc7b2a4c76e40e057a5fbc732
sofiaIG <user@users-MacBook-Pro.local>
2022-04-17 21:40:16 +0100

    Updates

---
+++ precache-manifest.15fbfa0216900125a511ff77b264f2c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd4a753843edfb3c28a1",
+    "url": "/personal_website/static/css/main.48ef6a37.chunk.css"
+  },
+  {
+    "revision": "fd4a753843edfb3c28a1",
+    "url": "/personal_website/static/js/main.fd4a7538.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/personal_website/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "e495a233d13d271953c2",
+    "url": "/personal_website/static/js/2.e495a233.chunk.js"
+  },
+  {
+    "revision": "027719e17721f0fb146c",
+    "url": "/personal_website/static/js/runtime~main.027719e1.js"
+  },
+  {
+    "revision": "0b8f896199b57e862a00966abb63e6ae",
+    "url": "/personal_website/static/media/sofia.0b8f8961.jpg"
+  },
+  {
+    "revision": "bd7f960e54345a9f9cb2a70daf694264",
+    "url": "/personal_website/index.html"
+  }
+];
