afa487605203aaec19e582fac939d5132c98141a
sofiaIG <sphignatiadi@gmail.com>
2022-06-30 16:18:28 +0100

    Add styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,29 @@
 import React from "react";
+import "../App.css"
 
 
 const Projects = ({ repositories }) => {
   return (
     <div id="projects">
       <h1>Projects</h1>
-      <ul className="links">
-        <h2>Projects that I have built before CodeClan</h2>
+      <h2>CodeClan Projects</h2>
+      <ul className="projects">
+        <li><a href="https://github.com/sofiaIG/Shop_Inventory">🌱 Plant Inventory</a></li>
+        <li><a href="https://github.com/sofiaIG/MSM-Shares">💸 MSM Shares</a></li>
+        <li><a href="https://github.com/sofiaIG/muse-app">🎵 Muse</a></li>
+      </ul>
+      <h2>Projects that I have built before CodeClan</h2>
+      <ul className="projects">
         <li><a href="https://github.com/sofiaIG/mortgage-calculator">
-          Morgage Calculator in Java
+        🏠 Morgage Calculator
         </a></li>
-        <li><a href="https://github.com/sofiaIG/Blog">Django Blog</a></li>
-        <li><a href="https://github.com/sofiaIG/TicTacToe">TicTacToe</a></li>
+        <li><a href="https://github.com/sofiaIG/Blog">🖊️ Blog</a></li>
+        <li><a href="https://github.com/sofiaIG/TicTacToe">🎲 TicTacToe</a></li>
         <li><a href="https://github.com/sofiaIG/FilmRatingFinder">
-          FilmRatingFinder
+        📽️ Film Rating Finder
         </a></li>
       </ul>
-        <ul>
-        <h2>CodeClan Projects</h2>
-        <li><a href="https://github.com/sofiaIG/Shop_Inventory">Plant Inventory</a></li>
-        <li><a href="https://github.com/sofiaIG/MSM-Shares">MSM Shares</a></li>
-        <li><a href="https://github.com/sofiaIG/muse-app">Muse</a></li>
-      </ul>
+
     </div>
   );
 };
